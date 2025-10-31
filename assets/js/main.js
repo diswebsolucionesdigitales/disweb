07cca2dbeb0c6c82b1d2ec403fde3865593cd793
diswebsolucionesdigitales <danympa@proton.me>
2025-10-31 00:51:55 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -110,19 +110,19 @@
   /**
    * Init typed.js
    */
-  const selectTyped = document.querySelector('.typed');
-  if (selectTyped) {
-    let typed_strings = selectTyped.getAttribute('data-typed-items');
-    typed_strings = typed_strings.split(',');
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000,
-      startDelay: 7500,
-    });
-  }
+ const selectTyped = document.querySelector('.typed');
+if (selectTyped) {
+  let typed_strings = selectTyped.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000,    // ✅ COMA INCLUIDA
+    startDelay: 7500    // ✅ SIN COMA AL FINAL
+  });                   // ✅ PARÉNTESIS CORRECTO
+}
 
   /**
    * Animate the skills items on reveal
